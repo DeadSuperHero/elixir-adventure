b2b3b883fff549ead9b7ebdc34867bf3eb607959
Sean Tilley <sean@deadsuperhero.com>
2018-07-02 02:01:25 -0700

    add formatting to story 2

--- lib/stories/story_2/story_2.ex
+++ lib/stories/story_2/story_2.ex
@@ -2,11 +2,18 @@ defmodule Story2 do
   require Verbs
 
   def engage() do
-    IO.puts "Deep Blue"
+    IO.puts "
+    ***************
+    *   Deep Blue  *
+    ****************
+
+    "
+    IO.puts "\n"
+    player_name = IO.gets "What is your name? "
     Process.sleep(1000)
+    Process.sleep(1000)
+    IO.puts "#{String.trim(player_name)}, you are swimming in the ocean."
     Verbs.prompt()
-    Process.sleep(1000)
-    IO.puts "You are swimming in the ocean."
   end
 
 end
