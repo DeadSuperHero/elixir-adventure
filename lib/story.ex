2f84aefb9b53e4351079e882973856d9a9ca97bb
Sean Tilley <sean@deadsuperhero.com>
2018-07-02 01:57:42 -0700

    randomize between two different stories on start

--- lib/story.ex
+++ lib/story.ex
@@ -11,12 +11,22 @@ defmodule Story do
     Process.sleep(1000)
     IO.puts "Examining Shroedinger's Cat..."
     Process.sleep(1000)
-    Story.randomize
+    Story.randomize(entry)
   end
 
-  def randomize() do
+  def entry do
     entry = Enum.random([1, 2, 3])
-    Story1.engage
+  end
+
+## This function will need to be refactored to increase more than two options.
+## For now, there are only two as a workaround.
+
+  def randomize(entry) do
+    if entry == 1 do
+      Story1.engage()
+    else
+      Story2.engage()
+    end
   end
 
 
